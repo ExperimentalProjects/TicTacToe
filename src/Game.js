cbefa499cd914aa103b03e88675cfa2e36915a7a
abhishek singh <abhishekcode1@gmail.com>
2020-01-19 14:50:11 +0530

    Auto play logic basic

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useReducer } from 'react';
 import { View, Text, TouchableOpacity, Button, StyleSheet } from 'react-native';
-import { checkForWin, reducer, initialState, PLAY_AND_CHANGE_TURN, RESET_GAME } from './logic';
+import { reducer, PLAY_AND_CHANGE_TURN, NEW_GAME, playAction, getIntialState } from './logic';
 
 
 const GameComponent = (props) => {
-    const [gameState, dispatch] = useReducer(reducer, initialState);
+    const [gameState, dispatch] = useReducer(reducer, getIntialState(props.gameType));
+    let isGameEnded = gameState.gameEnd && gameState.gameEnd.end
     return (
         <View style={[styles.column, { alignItems: 'center', paddingTop: 10 }]}>
             <View style={styles.row}>
@@ -22,15 +23,37 @@ const GameComponent = (props) => {
                 <GameTile id={8} gameState={gameState} dispatch={dispatch} />
                 <GameTile id={9} gameState={gameState} dispatch={dispatch} />
             </View>
+            {isGameEnded &&
+                <Text style={{ fontSize: 30 }}> Player {gameState.turn} Won!!</Text>
+            }
+            {!isGameEnded && gameState.filled &&
+                <Text style={{ fontSize: 30 }}> Game Drawn!</Text>
+            }
 
-            {gameState.gameEnd && gameState.gameEnd.end && <View style={{ alignItems: 'center' }}>
-                <Text style={{ fontSize: 30 }}> Game Over!</Text>
-                <Text style={{ fontSize: 30 }}> Player {gameState.turn} won!!!</Text>
+            {!isGameEnded && !gameState.filled && < View style={{ alignItems: 'center' }}>
+                <Text style={{ fontSize: 30 }}> Player {gameState.turn}'s Turn</Text>
             </View>}
+
+
+            <Text style={{ marginTop: 16, fontSize: 30 }}>Score</Text>
+            <View style={[styles.row, { width: '100%', justifyContent: "space-between", padding: 10, borderTopWidth: 1, marginTop: 16, alignItems: 'center' }]}>
+                <View style={styles.column}>
+                    <Text style={{ fontSize: 30 }}>Player X</Text>
+                    <Text style={{ fontSize: 30 }}>{gameState.score.X}</Text>
+                </View>
+                <View style={styles.column}>
+                    <Text style={{ fontSize: 30 }}>Player O</Text>
+                    <Text style={{ fontSize: 30 }}>{gameState.score.O}</Text>
+                </View>
+            </View>
+
+            {
+
+            }
             <TouchableOpacity
                 style={{ margin: 10, padding: 16, backgroundColor: 'blue', borderRadius: 4 }}
-                onPress={() => dispatch({ type: RESET_GAME })}>
-                <Text style={{ color: "white", fontSize: 20, fontWeight: 'bold' }}>Reset</Text>
+                onPress={() => dispatch({ type: NEW_GAME })}>
+                <Text style={{ color: "white", fontSize: 20, fontWeight: 'bold' }}>New Game</Text>
             </TouchableOpacity>
         </View >
     )
@@ -41,10 +64,15 @@ const GameTile = (props) => {
     const size = 100
     const { id, gameState, dispatch } = props
     let value = gameState[id]
+    let isGameEnded = gameState.gameEnd && gameState.gameEnd.end
     const isPartOfWinnigTile = gameState.gameEnd && gameState.gameEnd.ids.indexOf((id)) !== -1
     return (
         <TouchableOpacity
-            onPress={() => dispatch({ type: PLAY_AND_CHANGE_TURN, id })}
+            onPress={() => {
+                if (!value && !isGameEnded) {
+                    playAction(dispatch, id, gameState)
+                }
+            }}
             style={{ backgroundColor: isPartOfWinnigTile ? "green" : "white", borderWidth: 2, borderColor: 'black', width: size, height: size, justifyContent: 'center', alignItems: 'center' }}>
             <Text style={{ fontSize: 40, fontWeight: 'bold' }}>{value}</Text>
         </TouchableOpacity >
--- src/logic.js
+++ src/logic.js
@@ -1,54 +1,67 @@
+import { GAME_TYPE } from "./App";
+import { BorderlessButton } from "react-native-gesture-handler";
 
 
 
 
 export const PLAY_AND_CHANGE_TURN = "PLAY_AND_CHANGE_TURN"
-export const RESET_GAME = "RESET_GAME"
+export const NEW_GAME = "NEW_GAME"
 
 const TURN_MARK = {
     1: "X", 2: "O"
 }
-export const initialState = {
-    turn: 1
-};
 
 export function reducer(state, action) {
     switch (action.type) {
         case PLAY_AND_CHANGE_TURN:
-            let value = TURN_MARK[state.turn]
+            let value = state.turn
             let newState = { ...state, [action.id]: value };
+            console.log(newState)
             let gameEnd = checkForWin(newState, action.id, value)
             if (!gameEnd || !gameEnd.end) {
-                newState.turn = state.turn === 1 ? 2 : 1
+                //deciding turn
+                newState.turn = state.turn === TURN_MARK[1] ? TURN_MARK[2] : TURN_MARK[1]
             }
-            return { ...newState, gameEnd }
-        case RESET_GAME:
-            return initialState
+            if (gameEnd && gameEnd.end) {
+                // gameover score increment
+                newState.score[newState.turn] += 1
+            }
+
+            newState = { ...newState, gameEnd }
+            console.log("afterWin", newState)
+
+            if ((!gameEnd || !gameEnd.end) && newState.gameType === GAME_TYPE.BOT && newState.turn === newState.bot) {
+                // play for the bot
+                newState = botPlay(newState)
+            }
+            if (!notAllTilesAreFilled(newState)) {
+                //filled
+                newState.filled = true
+            }
+            return newState
+        case NEW_GAME:
+            let freshState = getIntialState(state.gameType)
+            return { ...freshState, score: state.score, gameType: state.gameType }
         default:
             throw new Error();
     }
 }
 
 export const checkForWin = (state, id, value) => {
-    let shouldCheckWin = true
-    if (shouldCheckWin) {
-        let rowCheck = checkValueInRow(state, id, value)
-        if (rowCheck.end) {
-            return rowCheck
-        }
 
-        let colCheck = checkValueInColumn(state, id, value)
-        if (colCheck.end) {
-            return colCheck
-        }
+    let rowCheck = checkValueInRow(state, id, value)
+    if (rowCheck.end) {
+        return rowCheck
+    }
 
-        let diaCheck = checkValueInDiagonal(state, id, value)
-        if (diaCheck.end) {
-            return diaCheck
-        }
+    let colCheck = checkValueInColumn(state, id, value)
+    if (colCheck.end) {
+        return colCheck
+    }
 
-    } else {
-        return false
+    let diaCheck = checkValueInDiagonal(state, id, value)
+    if (diaCheck.end) {
+        return diaCheck
     }
 }
 
@@ -92,11 +105,65 @@ const checkValueInDiagonal = (state, id, value) => {
     return {}
 }
 
-
-
-
-export const updateGameState = (id, gameState, update) => {
-    let newState = gameState ? gameState : {}
-    newState[id] = "X"
-    update(newState)
+const notAllTilesAreFilled = (state) => {
+    for (let i = 1; i < 10; i++) {
+        if (!state[i]) {
+            return true
+        }
+    }
+    return false
 }
+
+
+export const botPlay = (state) => {
+    if (notAllTilesAreFilled(state)) {
+
+        let blankIndex
+        do {
+            blankIndex = Math.floor(1 + (Math.random() * (9 - 1 + 1)));
+        } while (state[blankIndex])
+
+        state[blankIndex] = state.bot
+        state.turn = "X"
+        let gameEnd = checkForWin(state, blankIndex, state.bot)
+        if (gameEnd && gameEnd.end) {
+            // gameover score increment
+            state.score[state.bot] += 1
+        }
+
+        return { ...state, gameEnd }
+    } else {
+        return state
+    }
+}
+
+export const playAction = (dispatch, id) => {
+    dispatch({ type: PLAY_AND_CHANGE_TURN, id })
+
+}
+
+
+export const getIntialState = (gameType) => {
+
+    const initialState = {
+        turn: Math.random() < 0.5 ? TURN_MARK[1] : TURN_MARK[2], //start by selecting random player turn
+        score: { X: 0, O: 0 },
+        bot: "O"
+    };
+
+    let state = { ...initialState, gameType: gameType }
+    if (gameType === GAME_TYPE.BOT && state.turn === state.bot) {
+        let blankIndex
+        do {
+            blankIndex = Math.floor(1 + (Math.random() * (9 - 1 + 1)));
+        } while (state[blankIndex])
+
+        state[blankIndex] = state.bot
+        state.turn = "X"
+    }
+
+    console.log("init", state)
+    return state
+}
+
+
